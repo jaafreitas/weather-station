963fb6223c2744099ee4416b36ba550cd581b83f
João Adriano Freitas <jaafreitas@gmail.com>
2019-08-30 00:49:26 -0300

    Using local BMP280 library.

--- station/sensorBMP280.cpp
+++ station/sensorBMP280.cpp
@@ -1,4 +1,4 @@
-#include <Adafruit_BMP280.h>
+#include "src/Adafruit_BMP280_Library/Adafruit_BMP280.h"
 #include "sensorBMP280.h"
 #include "settings.h"
 
@@ -9,8 +9,7 @@ extern "C" {
 Adafruit_BMP280 bmp;
 
 os_timer_t sensorBMP280Timer;
-
-bool canReadSensorBMP280 = true;
+bool canReadSensorBMP280 = false;
 
 void sensorBMP280Read(void *parg) {
   canReadSensorBMP280 = true;
@@ -20,6 +19,7 @@ void setupSensorBMP280() {
   if (!bmp.begin()) {  
     Serial.println("BMP280 sensor not connected!");
   } else {
+    canReadSensorBMP280 = true;
     os_timer_disarm(&sensorBMP280Timer);
     os_timer_setfn(&sensorBMP280Timer, sensorBMP280Read, NULL);
     os_timer_arm(&sensorBMP280Timer, SENSOR_BMP280_READ_INTERVAL, true);
